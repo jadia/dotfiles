bfb2700fad3c03055a7e34be75d53a64b1fbdbf4
Nitish Jadia <nitish@jadia.dev>
2022-08-30 20:53:30 +0530

    more fixes
    Signed-off-by: Nitish Jadia <nitish@jadia.dev>

--- scripts/monitor-setup.sh
+++ scripts/monitor-setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+# Clean the monitor setup
+# rm -rf /tmp/monitor_setup
 #declare -a modes=( extended hdmi-only lcd-only mirrored )
-declare -a modes=( extended lcd-only )
-
+declare -a modes=( dual extended lcd-only )
 MONITOR_SETUP="/tmp/monitor_setup"
 I3_RESTART="/usr/bin/i3-msg restart"
 WALLPAPER_RESET="feh --bg-scale --randomize /home/nitish/dotfiles/wallpapers/*"
@@ -10,6 +11,11 @@ WALLPAPER_RESET="feh --bg-scale --randomize /home/nitish/dotfiles/wallpapers/*"
 LAPTOP_DISPLAY="eDP-1" #5402
 #SECOND_DISPLAY="HDMI-0" # 7567
 SECOND_DISPLAY="HDMI-1" #5402
+
+LEFT_DISPLAY="DP-1-1-8"
+RIGHT_DISPLAY="DP-1-1-1"
+
+
 DISPLAY_RESET="xrandr --output $LAPTOP_DISPLAY --auto --primary --dpi 96 --output $SECOND_DISPLAY --off"
 
 function get_index {
@@ -32,6 +38,14 @@ function switch_setup {
 			xrandr --auto && xrandr --output $SECOND_DISPLAY --primary --left-of $LAPTOP_DISPLAY
 			# xrandr --auto && xrandr --output $SECOND_DISPLAY --primary --above $LAPTOP_DISPLAY
 			echo "extended" > $MONITOR_SETUP
+			$I3_RESTART
+			$WALLPAPER_RESET
+
+		;;
+		"dual" )
+			xrandr --auto && xrandr --output $LEFT_DISPLAY --primary --left-of $RIGHT_DISPLAY
+			# xrandr --auto && xrandr --output $SECOND_DISPLAY --primary --above $LAPTOP_DISPLAY
+			echo "dual" > $MONITOR_SETUP
 			$I3_RESTART
 			$WALLPAPER_RESET
 
