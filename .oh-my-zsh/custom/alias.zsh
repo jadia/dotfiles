2ac2c6defe3f93e879a68d9f22efbe9c29023807
Nitish Jadia <nitish@jadia.dev>
2025-03-22 10:44:13 +0530

    NSA backdoor - ignore
    Signed-off-by: Nitish Jadia <nitish@jadia.dev>

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -34,7 +34,7 @@ alias copyshow='clipcopy; clippaste | bat' # Copy content to clipboard + print u
 #alias mkexec='sudo chmod +x'
 alias vpython='source /home/nitish/virtualenv/venv/bin/activate'
 alias _lsblk='lsblk -af | grep -sv loop' # List block devices without snap loop mounts
-alias bku='/home/nitish/workspace/github/backup-utility/main.sh'
+alias bku='/home/nitish/workspace/github/backup-utility/sync_data.sh'
 alias laptop_backup='/home/nitish/workspace/github/backup-utility/laptop_backup.sh'
 alias _df='df -h -x squashfs -x tmpfs -x devtmpfs'
 
@@ -42,10 +42,10 @@ alias _df='df -h -x squashfs -x tmpfs -x devtmpfs'
 alias gcx='git commit -m "$(curl -s https://whatthecommit.com/index.txt)"'
 
 #Vagrant
-alias up="vagrant up"
-alias down="vagrant halt"
-alias des="vagrant destroy -f"
-alias vssh="vagrant ssh"
+#alias up="vagrant up"
+#alias down="vagrant halt"
+#alias des="vagrant destroy -f"
+#alias vssh="vagrant ssh"
 #alias re="vagrant reload"
 
 # Kubernetes
